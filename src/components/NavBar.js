660c0955ea0f159d059f2b482d015b8b8cbeba6a
Alberto Sierra <Sierra.Alberto@iCloud.com>
2023-10-11 20:45:50 -0700

    completed list and keys

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,19 @@
-import React from "react";
-
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+    <div>
+      <nav>
+        <a href={`#${links[0]}`}>{links[0]}</a>
+      </nav>
+      <nav>
+        <a href={`#${links[1]}`}>{links[1]}</a>
+      </nav>
+      <nav>
+        <a href={`#${links[2]}`}>{links[2]}</a>
+      </nav>
+    </div>
+  );
 }
 
 export default NavBar;
